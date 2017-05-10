b57b2ba2d46036d745f583d231bcb82a117d8c6e
Unknown <teukuyusransyahtandi@gmail.com>
2017-05-10 10:21:23 +0700

    open kamera agar bisa digunakan untuk scan

--- app/src/main/java/com/example/asus/veggiesandfruitscanner/MainActivity.java
+++ app/src/main/java/com/example/asus/veggiesandfruitscanner/MainActivity.java
@@ -1,13 +1,23 @@
 package com.example.asus.veggiesandfruitscanner;
 
+import android.content.Intent;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void btnTakePhotoClicked(View view) {
+    Intent intent = new Intent();
+        intent.setClass(view.getContext(),ScannerActivity.class);
+        startActivity(intent);
     }
 }
