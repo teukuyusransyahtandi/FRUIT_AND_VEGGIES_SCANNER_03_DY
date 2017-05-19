e850b8473d35631583c41f066b37db931bb14b56
erte\bangfac_studio <fachry@mhs.unsyiah.ac.id>
2017-05-19 15:36:49 +0700

    detail dari scann

---
+++ Detail.java
@@ -0,0 +1,15 @@
+package com.example.asus.veggiesandfruitscanner;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.asus.veggiesandfruitscanner.R;
+
+public class Detail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+    }
+}
